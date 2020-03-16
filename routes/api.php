decda95b3ef7b4f2b9351aa155b582906e2c5c20
Nikola <nikolaviki@hotmail.com>
2020-03-16 23:07:11 +0100

    first

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('api')->get('/contacts', 'ContactController@index');
+Route::middleware('api')->post('/contacts', 'ContactController@store');
+Route::middleware('api')->get('/contacts/{id}', 'ContactController@show');
+Route::middleware('api')->put('/contacts/{id}', 'ContactController@update');
